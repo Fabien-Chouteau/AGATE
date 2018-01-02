2442bf5aeea1356d772f5a7acd837f4c027e4d02
Fabien Chouteau <fabien.chouteau@gmail.com>
2018-01-02 23:38:08 +0100

    AGATE.Interrupts: Add HardFault handler

--- src/agate-interrupts.adb
+++ src/agate-interrupts.adb
@@ -29,9 +29,11 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
-with Ada.Text_IO;
+with Ada.Text_IO;             use Ada.Text_IO;
 with HAL;                     use HAL;
+with Tools;                   use Tools;
 with System.Storage_Elements; use System.Storage_Elements;
+with System.Machine_Code;     use System.Machine_Code;
 with Cortex_M_SVD.SCB;        use Cortex_M_SVD.SCB;
 with Cortex_M_SVD.NVIC;       use Cortex_M_SVD.NVIC;
 
@@ -45,6 +47,85 @@ package body AGATE.Interrupts is
    procedure IRQ_Handler;
    pragma Export (C, IRQ_Handler, "__gnat_irq_trap");
 
+   procedure Hard_Fault_Handler;
+
+
+   ------------------------
+   -- Hard_Fault_Handler --
+   ------------------------
+
+   procedure Hard_Fault_Handler
+   is
+
+      function PSP
+        return System.Address
+      is
+         Ret : System.Address;
+      begin
+         Asm ("mrs %0, psp",
+              Outputs  => System.Address'Asm_Output ("=r", Ret),
+              Volatile => True);
+
+         return Ret;
+      end PSP;
+
+      type Stack_Array is array (1 .. 8) of UInt32
+        with Pack, Size => 8 * 32;
+
+      Context : Stack_Array
+        with Address => PSP;
+   begin
+      Put_Line ("In HardFault");
+      if SCB_Periph.HFSR.FORCED then
+         Put_Line ("Forced HardFault");
+
+         Put_Line ("Vector table read         : " & SCB_Periph.HFSR.VECTTBL'Img);
+
+         Put_Line ("MemManage:");
+         Put_Line (" - Instruction access     : " & SCB_Periph.MMSR.IACCVIOL'Img);
+         Put_Line (" - Data access            : " & SCB_Periph.MMSR.DACCVIOL'Img);
+         Put_Line (" - Unstacking from excp   : " & SCB_Periph.MMSR.MUNSTKERR'Img);
+         Put_Line (" - Stacking for excp      : " & SCB_Periph.MMSR.MSTKERR'Img);
+         Put_Line (" - FPU Lazy state preserv : " & SCB_Periph.MMSR.MLSPERR'Img);
+
+         if SCB_Periph.MMSR.MMARVALID then
+            Put_Line ("-> address : " & SCB_Periph.MMAR'Img);
+         end if;
+
+         Put_Line ("BusFault:");
+         Put_Line (" - Instruction bus error  : " & SCB_Periph.BFSR.IBUSERR'Img);
+         Put_Line (" - Precise data bus error : " & SCB_Periph.BFSR.PRECISERR'Img);
+         Put_Line (" - Imprecise data bus err : " & SCB_Periph.BFSR.IMPRECISERR'Img);
+         Put_Line (" - Unstacking from excp   : " & SCB_Periph.BFSR.UNSTKERR'Img);
+         Put_Line (" - Stacking for excp      : " & SCB_Periph.BFSR.STKERR'Img);
+         Put_Line (" - FPU Lazy state preserv : " & SCB_Periph.BFSR.LSPERR'Img);
+
+         if SCB_Periph.BFSR.BFARVALID then
+            Put_Line ("-> address : " & SCB_Periph.BFAR'Img);
+         end if;
+
+         Put_Line ("UsageFault:");
+         Put_Line (" - Undefined instruction  : " & SCB_Periph.UFSR.UNDEFINSTR'Img);
+         Put_Line (" - Invalid state          : " & SCB_Periph.UFSR.INVSTATE'Img);
+         Put_Line (" - Invalid PC load        : " & SCB_Periph.UFSR.INVPC'Img);
+         Put_Line (" - No co-processor        : " & SCB_Periph.UFSR.NOCP'Img);
+         Put_Line (" - Unaligned access       : " & SCB_Periph.UFSR.UNALIGNED'Img);
+         Put_Line (" - Division by zero       : " & SCB_Periph.UFSR.DIVBYZERO'Img);
+
+         Put_Line ("R0 : " & Hex (Context (1)));
+         Put_Line ("R1 : " & Hex (Context (2)));
+         Put_Line ("R2 : " & Hex (Context (3)));
+         Put_Line ("R3 : " & Hex (Context (4)));
+         Put_Line ("R12: " & Hex (Context (5)));
+         Put_Line ("LR : " & Hex (Context (6)));
+         Put_Line ("PC : " & Hex (Context (7)));
+         Put_Line ("PSR: " & Hex (Context (8)));
+         loop
+            null;
+         end loop;
+      end if;
+   end Hard_Fault_Handler;
+
    ----------------
    -- Initialize --
    ----------------
@@ -57,6 +138,7 @@ package body AGATE.Interrupts is
       Vector_Address : constant UInt32 := UInt32 (To_Integer(Vector'Address));
    begin
       SCB_Periph.VTOR := Vector_Address;
+      Register (Hard_Fault_Handler'Access, -13, 0);
    end Initialize;
 
    --------------
